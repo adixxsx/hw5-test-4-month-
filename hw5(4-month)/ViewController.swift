4ea5259c265673352cf47eaef53d93781e036f1d
adixxsx <ad1letsa5irkulov@gmail.com>
2023-12-26 00:04:00 -0800

    Initial Commit

---
+++ hw5(4-month)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw5(4-month)
+//
+//  Created by user on 26/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
